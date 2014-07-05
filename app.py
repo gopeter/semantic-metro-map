a6e00dac45c52a624577af3207a8f7eba52b926d
Peter Göbel <peter.goebel@debd.de>
2014-07-05 21:46:05 +0200

    added route to calculate shortest path of chosen map and start/end points

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 import mimetypes
+import json
 from flask import Flask, jsonify, render_template, request
 from bs4 import BeautifulSoup
 
@@ -18,58 +18,69 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def parseData():
-	g = nx.MultiDiGraph()
-	# open SVG file and extract RDF
-	with open ("metro.svg", "r") as metro:
-		data = metro.read()
-		xml = BeautifulSoup(data)
-		metadata = xml.find('metadata')
-		
-		# convert RDF/XML triples into networkx paths
-		for item in metadata.findAll('rdf:description'):
-		
-			# get start node and strip 'http://example.com/' from string
-			start = item['rdf:about'][19:]
-			nodes = {}
-			
-			# get connectsd nodes/objects
-			for node in item.findAll('ex:via'):
-				stop = node.find('ex:stop')['rdf:resource'][19:]
-				line = node.find('ex:line')['rdf:resource'][19:]
-				duration = float(node.find('ex:duration').string)
-				
-				# add edge to graph
-				g.add_edges_from([
-					(start, stop, {'duration': duration, 'line': line})
-				])
-	
-	# get total duration
-	length = nx.dijkstra_path_length(g,'StopD','StopC', 'duration')
+  g = nx.MultiDiGraph()
+  
+  # store post data
+  p_map = request.form['map']
+  p_start = request.form['start']
+  p_end = request.form['end']    
+  
+  # open SVG file and extract RDF
+  with open('static/maps/' + p_map, 'r') as metro:
+    data = metro.read()
+    xml = BeautifulSoup(data)
+    metadata = xml.find('metadata')
 
-	# get egdes to the shortest path
-	for edge in zip(path,path[1:]):                                                 
-	    data = g.get_edge_data(*edge)                                              
-	    print edge,min(data.iteritems(), key = lambda x : x[1]['duration'])
+    # convert RDF/XML triples into networkx paths
+    for item in metadata.findAll('rdf:description'):
+
+      # get start node and strip 'http://example.com/' from string
+      start = (item['rdf:about'])[19:]
+      nodes = {}
+
+      # get connectsd nodes/objects
+      for node in item.findAll('ex:via'):
+        stop = (node.find('ex:stop')['rdf:resource'])[19:]
+        line = (node.find('ex:line')['rdf:resource'])[19:]
+        duration = float(node.find('ex:duration').string)
+
+        # add edge to graph
+        g.add_edges_from([(start, stop, {'duration': duration, 'line': line})])
+
+  # get shortest path
+  path = nx.dijkstra_path(g, p_start, p_end, 'duration')
+
+  # get total duration
+  length = nx.dijkstra_path_length(g, p_start, p_end, 'duration')
+
+  # get egdes to the shortest path and return as JSON
+  results = []
+  for edge in zip(path, path[1:]):
+    data = g.get_edge_data(*edge)
+    results.append((edge, min(data.iteritems(), key=lambda x: x[1]['duration'])))
+
+  # return edges as json array
+  return json.dumps(results)
 
 ################################################################################
 # Serve maps
 ################################################################################
-	    
+
 @app.route('/maps/<filename>', methods = ['GET'])
 def maps(filename):
-	return app.send_static_file('maps/' + filename)
+  return app.send_static_file('maps/' + filename)
 
 ################################################################################
-# Serve index file 
+# Serve index file
 ################################################################################
-	    
+
 @app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 ################################################################################
 # Start app
 ################################################################################
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+  app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,27 @@
+var map = 'metro.svg'; // ability to change maps dynamically
+
 $(function() {
 
 	// load svg map
-	$.get('maps/metro.svg', null, function(data) {
+	$.get('maps/' + map, null, function(data) {
 		var el = $('svg', data);
 		var svg = document.adoptNode(el[0]);
 		$('#map').html(svg);
 	}, 'xml');
+	
+	// post foobar
+	$.ajax({
+		url: '/',
+		type: 'post',
+		data: {
+			map: map,
+			start: 'StopC',
+			end: 'StopF'
+		}, 
+		success: function(res) {
+			console.log(res);
+		}
+		
+	});
 	
 });
